f8ca7c1471cbf88354980ea916422409d6accbfc
ihufa <martin_grysbaek@hotmail.com>
2019-01-20 00:57:40 +0100

    20-01

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import Buy from './modals/buy'
 
 class Watchlist extends Component {
     constructor(props) {
         super(props)
         this.state={
             ticker: '',
-            volume: 0
+            volume: 0,
+            showBuyModal: false,
+
         }
     }
 
@@ -15,15 +18,32 @@ this.props.deleteStock(e.target.id)
 }
 showGraphHandler = (e) => {
   e.preventDefault()
-  this.props.showGraph(e.target.id)
+  console.log(e.target.name)
+  this.props.showGraph(e.target.name)
 }
 buyHandler = (e) => {
   e.preventDefault()
-  this.props.buyHandler(e.target.id)
+  this.setState({
+    ...this.state, showBuyModal: !this.state.showBuyModal, ticker: e.target.name
+  })
+}
+modalOK = () => {
+  this.setState({
+    showBuyModal: false
+  })
 }
 
+
   render() {
-    if (Object.keys(this.props.watchlist[0]).length !== 0) {     // dont render if watchlist is empty object
+    if (this.state.showBuyModal) { return <Buy modalMessage = "Enter Volume" ticker = {this.state.ticker} 
+        price = {this.props.watchlist[this.props.watchlist.findIndex(item => item.ticker === this.state.ticker)].prices[99]}
+      time={this.props.watchlist[this.props.watchlist.findIndex(item => item.ticker === this.state.ticker)].times[99]} 
+        handleBuy={this.props.handleBuy} modalOK={this.modalOK}
+        updatePortfolioData={this.props.updatePortfolioData}
+        handleClose={this.modalOK}
+        />}
+
+    if (Object.keys(this.props.watchlist[0] !== undefined && this.props.watchlist[0]).length !== 0) {     // dont render if watchlist is empty object
       return (<div>
         <h2>Watchlist</h2>
         <table className="watchlist">
@@ -31,11 +51,9 @@ buyHandler = (e) => {
           <tr><th>Stock ticker:</th><th>Price:</th></tr>
           </thead>
           <tbody>{this.props.watchlist.map(asset => 
-          (<tr key={asset.ticker}>
-            <td className="ticker">{asset.ticker}</td> <td className="price">${asset.prices[asset.prices.length-1]}</td>
-            <td><button className="btn" id={asset.ticker} onClick={this.showGraphHandler}>See Graph</button></td>
-            <td><button className="btn" id={asset.ticker} onClick={this.buyHandler}>Buy</button></td>
-            <td><button className="btn" id={asset.ticker} onClick={this.deleteHandler}>Remove from list</button></td>
+            (<tr key={asset.ticker} name={asset.ticker} onMouseOver={this.showGraphHandler}>
+              <td name={asset.ticker} className="ticker">{asset.ticker}</td><td className="price" name={asset.ticker}>${asset.prices[asset.prices.length-1]}</td>
+            <td><button className="Btn" name={asset.ticker} onClick={this.buyHandler}>Buy</button></td>
             </tr>)
           )}
           </tbody>
@@ -44,15 +62,11 @@ buyHandler = (e) => {
     )}
     else if(this.props.stocks.length > 0) {return (
       <div>
-        <h2>Watchlist</h2>
       <table className="watchlist">
-        <thead>
-          <tr><th>Stock ticker:</th><th>Price:</th></tr>
-        </thead>
       <tbody>{this.props.stocks.map(asset =>
         (<tr key={asset.ticker}>
           <td className="ticker">{asset.ticker}</td>
-          <td></td><td></td><td><button className="btn" id={asset.ticker} onClick={this.deleteHandler}>Remove from list</button></td>
+          <td><button className="Btn" id={asset.ticker} onClick={this.deleteHandler}>Remove from list</button></td>
         </tr>)
       )}
       </tbody>
--- src/components/modals/buy.js
+++ src/components/modals/buy.js
@@ -1,41 +1,67 @@
 import React, { Component } from 'react'
 
-class buy extends Component {
+class Buy extends Component {
     constructor(props){
         super(props)
         this.state = {
-            volme: 0
+            volume: 0,
+            timeOfPurchase: 0,
+            price: 0,
+            currentPrice: 0
         }
-
     }
+handleChange = (e) => {
+    this.setState({
+        volume: e.target.value,
+        ticker: this.props.ticker,
+        timeOfPurchase: this.props.time,
+        price: this.props.price,
+        currentPrice: this.props.price,
+        currentTime: this.props.price
+    })
+}
+
+handleBuy = (e) => {
+    e.preventDefault()
+    if(this.state.volume > 0) {this.props.handleBuy(this.state)}
+    this.props.modalOK()
+}
+handleClose = (e) => {
+    e.preventDefault()
+    console.log(e.target)
+    if(e.target.className !== 'modal')
+   { this.props.handleClose()}
+}
+    
   render() {
       const backgroundStyle = {
-          position: 'fixed',
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          backgroundColor: 'rgba(0,0,0,0.5)'
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'rgba(0,0,0,0.5)'
       }
       const modalStyle = {
-          backgroundColor: '#fff',
-          maxWitdh: 250,
-          height: 150,
-          margin: 'auto',
-          position: 'relative'
+        backgroundColor: '#fff',
+        width: '70%'    ,
+        height: 150,
+        margin: 'auto',
+        marginTop: 250,
+        position: 'relative'
       }
-      const fromStyle = {
-
+      const formStyle = {
+        
       }
 
 
     return (
-      <div style={backgroundStyle}>
-        <div style={modalStyle}>
-        <h3>{this.props.modalMessage}</h3>
-            <form style={formStyle}>
-                <input onChange={this.handleChange} type="number" placeholder="Volume"></input>
-                <button onClick={this.handleBuy}>Buy</button>
+      <div onClick={this.handleClose} style={backgroundStyle}>
+        <div className="modal" style={modalStyle}>
+        <h3 className="modal">{this.props.modalMessage}</h3>
+                <form className="modal" style={formStyle}><span>{this.props.ticker}</span>
+                    <input className="modal" onChange={this.handleChange} type="number" placeholder="Volume"></input> <span className="modal">{Math.round(this.props.price*this.state.volume)}$</span>
+                <button className="modal" onClick={this.handleBuy}>Buy</button>
             </form>
         </div>
       </div>
@@ -43,4 +69,4 @@ class buy extends Component {
   }
 }
 
-export default buy
+export default Buy
