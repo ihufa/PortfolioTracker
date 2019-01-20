469a03f7400fcf717ff9e1c38c5baf3ca11fed93
ihufa <martin_grysbaek@hotmail.com>
2019-01-20 01:32:10 +0100

    Updates

---
+++ precache-manifest.2ee7cc4cc1bee56da8842645724d4d0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "868af9ee95ef411e364265be9884b97b",
+    "url": "/myapp/static/media/background.868af9ee.jpg"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "5848ceac7dd41018f7be",
+    "url": "/myapp/static/js/main.5848ceac.chunk.js"
+  },
+  {
+    "revision": "03367ef29e6a1c6c09f5",
+    "url": "/myapp/static/js/1.03367ef2.chunk.js"
+  },
+  {
+    "revision": "5848ceac7dd41018f7be",
+    "url": "/myapp/static/css/main.24645169.chunk.css"
+  },
+  {
+    "revision": "783293325c29b1711abb26ed408c5b34",
+    "url": "/myapp/index.html"
+  }
+];
