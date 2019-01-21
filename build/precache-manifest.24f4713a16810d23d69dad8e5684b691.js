ce136b4bf3e3c90c250c7fe6b5369f0da048fe64
ihufa <martin_grysbaek@hotmail.com>
2019-01-21 23:49:18 +0100

    blacknorange theme

---
+++ build/precache-manifest.24f4713a16810d23d69dad8e5684b691.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "868af9ee95ef411e364265be9884b97b",
+    "url": "/PortfolioTracker/static/media/background.868af9ee.jpg"
+  },
+  {
+    "revision": "5acdc076a026b5b361ab",
+    "url": "/PortfolioTracker/static/js/runtime~main.5acdc076.js"
+  },
+  {
+    "revision": "e24bb6ca67a7e227735a",
+    "url": "/PortfolioTracker/static/js/main.e24bb6ca.chunk.js"
+  },
+  {
+    "revision": "ed2037b2db95c5060b3d",
+    "url": "/PortfolioTracker/static/js/1.ed2037b2.chunk.js"
+  },
+  {
+    "revision": "e24bb6ca67a7e227735a",
+    "url": "/PortfolioTracker/static/css/main.b81e2fb5.chunk.css"
+  },
+  {
+    "revision": "731805c0f8a58f3af6c5506deb87c1ee",
+    "url": "/PortfolioTracker/index.html"
+  }
+];
